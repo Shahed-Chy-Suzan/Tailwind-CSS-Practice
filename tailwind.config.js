29113b8c67e2547423263e5c31088cbc0d0dc3b8
Shahed Chy Suzan <shahedchysuzan@gmail.com>
2021-12-13 02:16:55 +0600

    #12 Tailwind Configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@ module.exports = {
   purge: [],
   darkMode: 'class', // or 'media' or 'class' //default vabe ( false ) likha chilo //'class' for dark mood on toogle
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: {
+          450: '#5F99F7'
+        },
+        'regal-blue': '#243c5a',
+      },
+    },
   },
   variants: {
     extend: {},
