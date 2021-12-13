cf52bd6897b069b678cd9b302baeb32ce393b91e
Shahed Chy Suzan <shahedchysuzan@gmail.com>
2021-12-13 19:57:53 +0600

    #14 Just in Time

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-  purge: [],
+  mode: 'jit',        //for enabling 'Just In Time'(JIT) mode
+  purge: [
+    '*.html',                           //project er root e index.html file ta ke track korbe
+    './public/**/*.html',							  // These paths are just examples, customize them to match your project structure
+    './src/**/*.{js,jsx,ts,tsx,vue}',		//details opore likha hoyeche txt file e
+  ],
   darkMode: 'class', // or 'media' or 'class' //default vabe ( false ) likha chilo //'class' for dark mood on toogle
   theme: {
     extend: {
